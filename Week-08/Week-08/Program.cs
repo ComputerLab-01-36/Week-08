f7e21829d55e43e3d1837e272d211297f19abd63
Toeyty <61030236@kmitl.ac.th>
2019-11-01 14:59:21 +0700

    3.2.3 การล้างข้อมูลในคิวด้วยเมธอด Clear()

--- Week-08/Week-08/Program.cs
+++ Week-08/Week-08/Program.cs
@@ -10,7 +10,16 @@ namespace Week_08
     {
         static void Main(string[] args)
         {
+            Queue<string> queue1 = new Queue<string>();
+            queue1.Enqueue("RED");
+            queue1.Enqueue("BLUE");
+            queue1.Enqueue("YELLOW");
+            queue1.Enqueue("GREEN");
 
+            Console.WriteLine("The elements in the queue are:" + queue1.Count);
+            queue1.Clear();
+            Console.WriteLine("The elements in the queue are after the clear method:" ,"+ queue1.Count");
+            Console.ReadLine();
         }
     }
 }
