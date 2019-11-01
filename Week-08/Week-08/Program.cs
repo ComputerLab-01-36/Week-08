e246774848abb835a07d4d888140a26e60e201c8
Toeyty <61030236@kmitl.ac.th>
2019-11-01 16:30:07 +0700

    a4

--- Week-08/Week-08/Program.cs
+++ Week-08/Week-08/Program.cs
@@ -10,7 +10,21 @@ namespace Week_08
     {
         static void Main(string[] args)
         {
-
+            int[] data = new int[10];
+            data[0] = 0;
+            data[1] = 1;
+            data[2] = 2;
+            data[3] = 3;
+            data[4] = 4;
+            data[5] = 5;
+            data[6] = 6;
+            data[7] = 7;
+            data[8] = 8;
+            data[9] = 9;
+            
+            Console.WriteLine(data[6]);
+            
+            Console.ReadLine();
         }
     }
 }
