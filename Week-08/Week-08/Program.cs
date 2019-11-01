8895148be5376127d87d09972531213ad20b3d4b
Toeyty <61030236@kmitl.ac.th>
2019-11-01 14:48:09 +0700

    3.1.3 การเข้าถึงสมาชิกในอาร์เรย์ลิสต์แบบต่างๆ

--- Week-08/Week-08/Program.cs
+++ Week-08/Week-08/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,25 @@ namespace Week_08
     {
         static void Main(string[] args)
         {
+            ArrayList list1 = new ArrayList();
+            list1.Add(10);
+            list1.Add(20);
+            list1.Add(30);
+            list1.Add(40);
+            for (int i = 0; i < list1.Count; i++)
+            {
+                Console.WriteLine(list1[i]);
+            }
+            ArrayList list2 = new ArrayList(list1);
+            list2[2] = 55;
+            list2[3] = 66;
+            list2[4] = 77;
 
+            for (int i = 0; i < list2.Count; i++)
+            {
+                Console.WriteLine(list2[i]);
+            }
+            Console.ReadLine();
         }
     }
 }
