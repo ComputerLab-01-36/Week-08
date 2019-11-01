3e94a73a018bec918e44cd5b0b03a2b80b993953
Toeyty <61030236@kmitl.ac.th>
2019-11-01 14:20:19 +0700

    การตรวจสอบจำนวนสมาชิกในอาร์เรย์

--- Week-08/Week-08/Program.cs
+++ Week-08/Week-08/Program.cs
@@ -10,7 +10,21 @@ namespace Week_08
     {
         static void Main(string[] args)
         {
-
+            int[] data = new int[10];
+            Student[] student = new Student[31];
+            Console.WriteLine(data.Length);
+            Console.WriteLine(student.Length);
+            Console.ReadLine();
         }
     }
 }
+class Student     // nested class
+{
+    private string name;
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+}
+    
