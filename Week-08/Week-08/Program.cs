4296e5f11135188ba92d2d53295e72c101f9110b
Toeyty <61030236@kmitl.ac.th>
2019-11-01 16:23:51 +0700

    จังหวัด

--- Week-08/Week-08/Program.cs
+++ Week-08/Week-08/Program.cs
@@ -8,8 +8,19 @@ namespace Week_08
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
+
+            string[] changwad = new string[78];
+
+            Console.WriteLine("\r\n------ changwad ------\r\n");
+            for (int i = 0; i < changwad.Length; i++)
+            {
+                Console.Write("chandwat[" + i + "] : ");
+                changwad[i] = Console.ReadLine().ToString();
+            }
+
 
         }
     }
