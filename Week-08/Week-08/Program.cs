9d71d8b4fe819a7dd2f4f323e48e6052e0c834fd
Toeyty <61030236@kmitl.ac.th>
2019-11-02 14:24:51 +0700

    B2

--- Week-08/Week-08/Program.cs
+++ Week-08/Week-08/Program.cs
@@ -8,9 +8,39 @@ namespace Week_08
 {
     class Program
     {
+        
+
         static void Main(string[] args)
         {
 
+            int[] month = { 2,5,5,1,3,6,1,4,0,2,5,0};
+            string[] days =
+            {
+                "sun",
+                "Mon",
+                "Tue",
+                "Wed",
+                "Thu",
+                "Fri",
+                "Sat",
+            };
+            Console.Write("enter date : ");
+            int date = int.Parse(Console.ReadLine());
+            Console.Write("enter month:");
+            int inmonth = int.Parse(Console.ReadLine());
+
+            int day = (date + month[inmonth - 1] - 1) % 7;
+
+            Console.WriteLine($"{date}/{month}/2561 is {days[day]}");
+            Console.ReadLine();
+
+
+
+
+
+            
         }
+
+       
     }
 }
