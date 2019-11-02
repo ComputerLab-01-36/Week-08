530cc151c753813436533ac2e5fa8bc0d9a24a87
Toeyty <61030236@kmitl.ac.th>
2019-11-02 12:15:29 +0700

    .

--- Week-08/Week-08/Program.cs
+++ Week-08/Week-08/Program.cs
@@ -12,13 +12,13 @@ namespace Week_08
         {
             //หาgpa
             int[] data = new int[8];
-            data[0] = 4*3;
-            data[1] = 4*3;
-            data[2] = 4*3;
-            data[3] = 4*3;
-            data[4] = 4*3;
-            data[5] = 4*3;
-            data[6] = 4*3;
+            data[0] = 4*1;
+            data[1] = 4*1;
+            data[2] = 4*1;
+            data[3] = 4*1;
+            data[4] = 4*1;
+            data[5] = 4*1;
+            data[6] = 4*1;
             data[7] = 4*1;
             
             Console.WriteLine(data[0]);
@@ -29,8 +29,9 @@ namespace Week_08
             Console.WriteLine(data[5]);
             Console.WriteLine(data[6]);
             Console.WriteLine(data[7]);
-            data = data[0] + data[1] + data[2] + data[3] + data[4] + data[5] + data[5] + data[5] + data[6] + data[7];
-            
+
+
+            Console.WriteLine(value: data[0 + 1 + 2 + 3 + 4 + 5 + 6 + 7] );
             Console.ReadLine();
 
         }
