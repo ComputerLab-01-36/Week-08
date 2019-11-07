379083826a8183ae81de4afe5951ef90e6afa687
Toeyty <61030236@kmitl.ac.th>
2019-11-07 20:36:45 +0700

    commit

--- Week-08/Week-08/Program.cs
+++ Week-08/Week-08/Program.cs
@@ -10,7 +10,23 @@ namespace Week_08
     {
         static void Main(string[] args)
         {
-            
+            float[] gps = new float[8];
+            for (int i = 0; i < gps.Length; i++)
+            {
+                Console.Write($"Enter GPS term{i + 1} : ");
+                gps[i] = float.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("about GPS");
+
+            foreach (float j in gps)
+            {
+                Console.Write($"{j,0:N} ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("GPA is {0 , 0:N}", gps.Sum() / gps.Length);
+            Console.ReadLine();
         }
     }
 }
